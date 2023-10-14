e8e849f59cd457242e2cdebe7e348fa801756020
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-10-14 09:48:14 +0530

    Create Operators.cs

---
+++ Operators.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(5+8);
+        Console.WriteLine(50);
+        Console.WriteLine(5 * 8);
+        Console.WriteLine(5 / 8);
+        Console.WriteLine(5 % 2);
+        Console.WriteLine(4 + 2 * 3);
+        Console.WriteLine((4 + 2) * 3);
+        
+        Console.WriteLine(5 + 8.1);
+        
+        int num = 1;
+        num++;
+        Console.WriteLine(num);
+        
+        num--;
+        Console.WriteLine(num);
+    }
+}
