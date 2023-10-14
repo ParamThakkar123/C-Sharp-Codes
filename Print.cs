98bde5466cd732560512e1a1fe9c4ba513bb384b
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-10-14 09:30:18 +0530

    Create Print.cs

---
+++ Print.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("There once was a man named George.");
+        Console.WriteLine("He was 70 years old.");
+        Console.WriteLine("He liked the name George");
+        Console.WriteLine("But did not like being 70");
+    }
+}
