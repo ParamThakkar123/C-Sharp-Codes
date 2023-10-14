72a367553fc5ebca3a50d88446a8596322115f73
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-10-14 09:51:43 +0530

    Create Mathoperations.cs

---
+++ Mathoperations.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine(Math.Abs(-40));
+       
+       Console.WriteLine(Math.Pow(3, 2));
+       Console.WriteLine(Math.Sqrt(36));
+       Console.WriteLine(Math.Max(4, 60));
+       Console.WriteLine(Math.Round(4.3));
+       
+    }
+}
