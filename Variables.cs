7ac06d9be725ebd4651eda53789675d33a8d85a5
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-10-14 09:33:23 +0530

    Create Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string characterName = "John";
+        int age = 70;
+        
+        Console.WriteLine("There once was a man named " + characterName);
+        Console.WriteLine("He was " + age + " years old.");
+        Console.WriteLine("He liked the name " + characterName);
+        Console.WriteLine("But did not like being " + age);
+    }
+}
