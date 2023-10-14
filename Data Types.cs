1cb895d6507f8edfe4dfe4d97d4d394d800f8c63
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-10-14 09:36:34 +0530

    Create Data Types.cs

---
+++ Data Types.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string Name = "Param Thakkar";
+        char grade = 'A';
+        int age = 19;
+        double CGPA = 8.0;
+        bool isMale = true;
+        
+        Console.WriteLine(Name);
+        Console.WriteLine(grade);
+        Console.WriteLine(age);
+        Console.WriteLine(CGPA);
+        Console.WriteLine(isMale);
+    }
+}
